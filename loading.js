f4bc8a189dd1c5d8b3e3e85b1ef5e9cf000a65d6
412407512 <soso.412407512@gmail.com>
2024-12-03 23:18:38 +0200

    loading styleUpdate

--- loading.js
+++ loading.js
@@ -1,11 +1,12 @@
 let loading_website= document.querySelector('.loading-website');
 
+document.body.classList.add('overflow');
 window.addEventListener('load', function() {
     
     let pageLoadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
     console.log("Page loaded in: " + pageLoadTime + "ms");
     
     // Hide the loading screen after the page is fully loaded
-    document.body.style=`overflow-y:scroll`  
+    document.body.classList.remove('overflow')
     loading_website.style=`opacity:0; visibility:hidden`;
 });
